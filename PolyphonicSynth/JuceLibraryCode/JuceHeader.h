75552624023de9dcc3e0534147efb6a00bf439d3
thatissogabe <gabrielboquiren@gmail.com>
2017-09-26 17:18:19 -0700

    Project Version set
    Initial version set to 0.1.0

--- PolyphonicSynth/JuceLibraryCode/JuceHeader.h
+++ PolyphonicSynth/JuceLibraryCode/JuceHeader.h
@@ -38,7 +38,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "PolyphonicSynth";
-    const char* const  versionString  = "1.0.0";
-    const int          versionNumber  = 0x10000;
+    const char* const  versionString  = "0.1.1";
+    const int          versionNumber  = 0x101;
 }
 #endif
