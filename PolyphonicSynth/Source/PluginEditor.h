a8653ffe3e0d34302f3d9a7d597d97c6ebf6bcc7
AndrewCloud01 <andrewcloud01@gmail.com>
2017-10-24 22:50:08 -0700

    Minor Addition
    Nothing Much.

--- PolyphonicSynth/Source/PluginEditor.h
+++ PolyphonicSynth/Source/PluginEditor.h
@@ -14,7 +14,7 @@
 #include "PluginProcessor.h"
 #include <iostream>
 using namespace std;
-
+// New
 
 //==============================================================================
 /** This is the editor component that our filter will display.
